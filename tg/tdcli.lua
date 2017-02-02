c985dbf0d7426489bf87cbab85d35a8e3f470ada
spam2017 <spam_2@yahoo.com>
2017-02-02 18:22:02 +0330

    Update tdcli.lua

--- tg/tdcli.lua
+++ tg/tdcli.lua
@@ -2704,7 +2704,5 @@ M.sendForwarded = sendForwarded
 return M
 
 -- کد های پایین در ربات نشان داده نمیشوند
--- http://permag.ir
--- @permag_ir
--- @permag_bots
--- @permag
+-- Xx_mrIRANI_xX
+-- @delete_tg
